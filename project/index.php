70e67f387917541ebb3cb88da58fa81fe33367f5
jinzeld <Jinhui.zhen21@gmail.com>
2024-11-26 13:50:13 -0800

    added index file

---
+++ project/index.php
@@ -0,0 +1,28 @@
+<?php 
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gym mamagment system sql</title>
+</head>
+<body>
+<?php
+        // Include config file
+        require_once "config.php";
+        // include "header.php";
+	?>
+
+    <div class = "wapper">
+        <div class = "container">
+        <p> Gym managment system</p>
+        
+        </div>
+    </div>
+
+</body>
+</html>
